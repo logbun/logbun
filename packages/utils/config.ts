394a9fca76e0a688347c05aebe5cd6785ad86733
Meeky Dekowski <meekscreativelab@gmail.com>
2024-01-10 17:41:37 +0800

    js emphasize

--- packages/utils/config.ts
+++ packages/utils/config.ts
@@ -1,6 +1,6 @@
 export const siteConfig = {
   name: 'Logbun',
-  tagline: 'Simple, Open source, privacy-friendly error tracking for saas founders who ship fast.',
-  description: 'Open source error tracking platform for saas founders with a focus on privacy.',
+  tagline: 'Simple, Open source, privacy-friendly javascript error tracking for saas founders who ship fast.',
+  description: 'Open source javascript error tracking platform for saas founders with a focus on privacy.',
   url: 'https://logbun.com',
 };
