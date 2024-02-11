bb52e99730de6ebe0fa1614ae980c702dc58c597
Meeky Dekowski <meekscreativelab@gmail.com>
2024-02-11 11:24:20 +0800

    add external deps to node sdk rollup config

--- sdks/node/rollup.config.mjs
+++ sdks/node/rollup.config.mjs
@@ -3,10 +3,12 @@ import resolve from '@rollup/plugin-node-resolve';
 import replace from '@rollup/plugin-replace';
 import pkg from './package.json' assert { type: 'json' };
 
+const external = [...Object.keys(pkg.dependencies), 'http', 'https', '@aws-sdk/client-s3'];
+
 export default [
   {
     input: 'dist/index.js',
-    external: ['http', 'https'],
+    external,
     output: { file: pkg.main, format: 'cjs', sourcemap: true },
     plugins: [
       replace({ preventAssignment: false, exclude: 'node_modules/**', values: { __VERSION__: pkg.version } }),
