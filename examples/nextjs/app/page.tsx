660ceb6386f424bb8fb4be71b8836ef8e74ff82a
Meeky Dekowski <meekscreativelab@gmail.com>
2024-02-13 18:03:47 +0800

    update nextjs example

--- examples/nextjs/app/page.tsx
+++ examples/nextjs/app/page.tsx
@@ -1,16 +1,29 @@
 'use client';
+import { Logbun } from '@logbun/nextjs';
 
 export default function Home() {
+  function handledError() {
+    try {
+      // @ts-ignore
+      console.log(THIS_VARIABLE_DOES_NOT_EXIST);
+    } catch (e) {
+      Logbun.notify(e);
+    }
+  }
+
   function unhandledError() {
-    let number = 1;
-    (number as any).toLowerCase();
+    const number = 1;
+    // @ts-ignore
+    number.toUpperCase();
   }
 
   return (
     <>
-      <h1>Logbun Nextjs Example</h1>
+      <h1>Logbun React Example</h1>
       <div className="card">
-        <button onClick={unhandledError}>throw unhandledError</button>
+        <button onClick={handledError}>Logbun.notify(error)</button>
+        <button onClick={unhandledError}>throw new Error(error)</button>
+        <p>Handled by ErrorBoundary in main.jsx</p>
       </div>
     </>
   );
