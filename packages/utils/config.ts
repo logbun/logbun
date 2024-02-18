81a8ef3dbf8b0928e12d0bd2b0d32c651c9b2b0b
Meeky Dekowski <meekscreativelab@gmail.com>
2024-02-18 13:34:12 +0800

    fix png to jpg

--- packages/utils/config.ts
+++ packages/utils/config.ts
@@ -22,13 +22,13 @@ export const twitter = {
   title: site.title,
   description: site.description,
   card: 'summary_large_image',
-  images: [{ url: `${site.url}/og.png`, alt: defaultMetadata.title.default }],
+  images: [{ url: `${site.url}/og.jpg`, alt: defaultMetadata.title.default }],
 };
 
 export const openGraph = {
   title: site.title,
   description: site.description,
   type: 'website',
-  images: [{ url: `${site.url}/og.png`, width: 1200, height: 628, alt: defaultMetadata.title.default }],
+  images: [{ url: `${site.url}/og.jpg`, width: 1200, height: 628, alt: defaultMetadata.title.default }],
   siteName: site.title,
 };
